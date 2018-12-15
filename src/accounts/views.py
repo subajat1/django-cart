cdb0114d83898de0c05e5b2d2e14e493d52aade3
subajat1 <bayumunajat@outlook.com>
2018-12-15 21:48:55 +0800

    develop profile view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from shopping_cart.models import Order
+from .models import Profile
+
+
+def my_profile(request):
+	my_user_profile = Profile.objects.filter(user=request.user).first()
+	my_orders = Order.objects.filter(is_ordered=True, owner=my_user_profile)
+	context = {
+		'my_orders': my_orders
+	}
+
+	return render(request, "profile.html", context)
