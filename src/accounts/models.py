de418bae8538aaf254d12081440eb33fcf16be1f
subajat1 <bayumunajat@outlook.com>
2018-12-15 21:45:50 +0800

    link product model to profile model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 
+from products.models import Product
+
 User = get_user_model()
 
 class Profile(models.Model):
