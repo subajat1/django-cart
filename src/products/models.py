0814a878af2484e2cac79219184e6835bd3b4468
subajat1 <bayumunajat@outlook.com>
2018-12-15 22:03:21 +0800

    fix typo in product model

--- src/products/models.py
+++ src/products/models.py
@@ -4,5 +4,5 @@ class Product(models.Model):
     name = models.CharField(max_length=120)
     price = models.IntegerField()
 
-    def __str(self)__:
+    def __str__(self):
         return self.name
